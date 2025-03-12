54310daf9d5ac2854a9695fdaefb450e9abbbd22
Gowtham <123299351+GowthamM46@users.noreply.github.com>
2025-03-12 15:55:03 +0530

    Delete RestApi directory

--- RestApi/src/main/java/com/web/repository/RepositoryRestApi.java
+++
@@ -1,9 +0,0 @@
-package com.web.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.web.model.ModelRestApi;
-
-public interface RepositoryRestApi extends JpaRepository<ModelRestApi,String>{
-
-}
